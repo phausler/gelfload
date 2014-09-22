d71ee96b0111c0d86623d1c7bef88632ad4b5c5f
Philippe Hausler <phausler@gmail.com>
2014-09-22 14:48:51 -0700

    add a default symbol mapping for gmon

---
+++ src/elf_syms.c
@@ -0,0 +1,14 @@
+//
+//  elf_syms.c
+//  gelfload
+//
+//  Created by Philippe Hausler on 9/22/14.
+//  Copyright (c) 2014 Philippe Hausler. All rights reserved.
+//
+
+#include "elf_syms.h"
+
+void __gmon_start__(void)
+{
+    
+}
---
+++ src/elf_syms.h
@@ -0,0 +1,14 @@
+//
+//  elf_syms.h
+//  gelfload
+//
+//  Created by Philippe Hausler on 9/22/14.
+//  Copyright (c) 2014 Philippe Hausler. All rights reserved.
+//
+
+#ifndef __gelfload__elf_syms__
+#define __gelfload__elf_syms__
+
+#include <stdio.h>
+
+#endif /* defined(__gelfload__elf_syms__) */
